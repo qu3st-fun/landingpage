1390fab8d921914b52f021acbc8f12bb34bcc31d
Michael Schaefer <mischa.robots@gmail.com>
2025-05-14 16:08:35 +0200

    Add logo to app menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
 
+export const metadata = {
+  title: 'Qu3st.fun',
+  description: 'Qu3st.fun landingpage',
+  icons: {
+    icon: '/images/logo.ico',
+  },
+};
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
